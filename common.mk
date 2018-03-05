b42f324c31c704ff259d86e5f54ec3ee554cad47
tsnorri <tsnorri@iki.fi>
2018-03-05 15:09:01 +0200

    Add -Wno-misleading-indentation conditionally

--- common.mk
+++ common.mk
@@ -1,13 +1,19 @@
 # Ignore Xcode's setting since the SDK may contain older versions of Clang and libc++.
 unexport SDKROOT
 
-WARNING_FLAGS	= -Wall -Werror -Wno-deprecated-declarations -Wno-unused -Wno-misleading-indentation
+WARNING_FLAGS	= -Wall -Werror -Wno-deprecated-declarations -Wno-unused
 OPT_FLAGS		= -O2 -g
 
 CFLAGS			= -std=c99   $(OPT_FLAGS) $(WARNING_FLAGS)
 CXXFLAGS		= -std=c++1z $(OPT_FLAGS) $(WARNING_FLAGS)
 CPPFLAGS		= -I../include -I../lib/libbio/include -I../lib/sdsl-lite/include $(BOOST_INCLUDE)
 LDFLAGS			= ../lib/libbio/src/libbio.a $(LIBDISPATCH_LIBS) $(BOOST_LIBS) -lz
+
+COMPILER_CONSIDERS_MISLEADING_INDENTATION ?= 0
+
+ifeq (1,$(COMPILER_CONSIDERS_MISLEADING_INDENTATION))
+	WARNING_FLAGS += -Wno-misleading-indentation
+endif
 
 ifeq ($(shell uname -s),Linux)
 	#CPPFLAGS    += -I../lib/swift-corelibs-libdispatch
